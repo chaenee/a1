3e0258cce9f9f029c793efd2a0eef96f4984af01
chaenee <dbcodms05@gmail.com>
2017-08-11 02:33:22 -0700

    aaaaaaaaa

--- teamB/sensor/Sensor.py
+++ teamB/sensor/Sensor.py
@@ -173,7 +173,7 @@ class SensorServer(Thread):
 
             logger.info("Reading {} sensor...".format(self.sensor_names[3]))
             c6, c7 = self.read_sensor(3)
-            sn3 = ((c6 - 333) - (0.06 * c7 - (c7 - 274))) * 3.47222222
+            sn3 = ((c6 - 333) - (0.26 * c7 - (c7 - 274))) * 3.47222222
             sn3 = sn3 if (sn3 >= 0) else -sn3
             logger.info("{} sensor outputs {} ppb".format(self.sensor_names[3], sn3))
             # Save output to the dict
@@ -181,7 +181,7 @@ class SensorServer(Thread):
 
             logger.info("Reading {} sensor...".format(self.sensor_names[4]))
             c8, c9 = self.read_sensor(4)
-            sn4 = ((c8 - 418) - (0.38 * c9 - (c9 - 404))) * 2.54452926
+            sn4 = ((c8 - 418) - (0.08 * c9 - (c9 - 404))) * 2.54452926
             sn4 = sn4 if (sn4 >= 0) else -sn4
             logger.info("{} sensor outputs {} ppb".format(self.sensor_names[4], sn4))
             # Save output to the dict
